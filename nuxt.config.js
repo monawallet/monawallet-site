17d2bf1a23bd380e5226d2d6703258fb7ed072eb
nyanko-kota <mail@nyanko-kota.com>
2019-10-21 14:58:03 +0900

    [fix] ヘッダータイトルを修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,10 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+      { hid: 'MonaWallet',
+        name: 'MonaWallet',
+        content: 'Let`s enjoy with Mona！ MonaWalletはシンプルなモナコインウォレットです。',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
